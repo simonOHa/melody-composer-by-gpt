1b1b2a09b4681e9eb7cdbba1adaff8cb0086f409
simonharel <simonolivierharel@gmail.com>
2023-02-28 15:53:15 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + './dist/melody-composer-by-gpt'));
+app.use(express.static(__dirname + '/dist/melody-composer-by-gpt'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/melody-composer-by-gpt/index.html'));});
